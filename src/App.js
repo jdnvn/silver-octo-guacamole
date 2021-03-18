b1c29ce4359a2cc683e84830c59e198855c54fc1
Joey Donovan <josmdonovan@umass.edu>
2021-03-18 18:41:18 -0400

    created react app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from '@material-ui/core';
+import green from '@material-ui/core/colors/green';
 
 function App() {
+  const grn = green['A400'];
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button variant="contained" color="grn">Connect with Spotify</Button>
       </header>
     </div>
   );
